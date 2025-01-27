10e757418a31a8ccb84ab8d221ce23578638ba10
Dave Webb <dave.webb@cranbim.com>
2025-01-27 11:51:50 +0000

    Update sketch.js

--- oneShape/sketch.js
+++ oneShape/sketch.js
@@ -7,7 +7,7 @@ function setup() {
   }
   colorMode(HSB)
   // frameRate(1)
-  describe('A continually drawing line made of triangles in one of two colours that reflect the orintation c/w the previous triangle');
+  describe('A continually drawing line made of triangles in one of two colours that reflect the orintation c/w the previous triangle', LABEL);
 }
 
 function draw() {
